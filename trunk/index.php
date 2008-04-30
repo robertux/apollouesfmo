0faa61d04d3f5291fcddb264cf3b5c2fe7c17c95
BlendBoy <BlendBoy@68395a18-3848-0410-a3f5-bf879e04881c>
2008-04-30 20:25:08 +0000

    Agregados los posibles layouts del sitio. Agregada una hoja de estilo que definira la apariencia y distribucion del contenido del index.php. Modificado el index para que tome de referencia el .css que se esta desarrollando. Agregado un fondo que sirva como marco para el sitio.

--- trunk/index.php
+++ trunk/index.php
@@ -1,1 +1,15 @@
+<!DOCTYPE html PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html>
+    <head>
+        <meta name="generator" content="HTML Tidy, see www.w3.org">
+		<meta content="text/html">
+        <title>UES FMO - Unidad de Postgrados</title>
+		<link type="text/css" rel="stylesheet" href="Media/main.css">
+    </head>
+    <body>
+    	<div id="mainContent">
+    		prueba de contenido
+    	</div>
+    </body>
+</html>
 
