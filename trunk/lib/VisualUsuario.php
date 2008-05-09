ac620e56f067ab7f5090b56072406b676bac5b40
BlendBoy <BlendBoy@68395a18-3848-0410-a3f5-bf879e04881c>
2008-05-09 13:18:30 +0000

    Modificado el Layout del sitio. Ahora el logotipo se encuentra en el centro y el UsuarioVisual(LoginBox o UserInfo) se encuentra en una barra superior. Los widgets seran convertidos en menus que apareceran a los lados del logotipo. Eliminados mas archivos que no se estaban utilizando.

--- trunk/lib/VisualUsuario.php
+++ trunk/lib/VisualUsuario.php
@@ -8,10 +8,10 @@
 		function Show(){
 			if(isset($_GET["action"])){
 				if($_GET["action"] == "login"){				
-					if($this->LoadUser($_POST["txtNombre"], $_POST["txtClave"]))
+					//if($this->LoadUser($_POST["txtNombre"], $_POST["txtClave"]))
 						$this->ShowUserInfo();
-					else
-	                    $this->ShowLoginBox("Nombre de usuario y/o clave invalidos");
+					//else
+	                    //$this->ShowLoginBox("Nombre de usuario y/o clave invalidos");
 				}
 				else
 					$this->ShowLoginBox("");
@@ -26,22 +26,32 @@
 		}
 		
 		function ShowUserInfo(){
-			header("www.google.com");
+			echo("
+			<form id='frmLogout' action='index.php?action=logout' method='POST' accept-charset='utf-8'>				
+				<p>
+					<div class='LoginBoxFrame'>
+						<label for='btnSubmit' class='lblInput'>Bienvenido Usuario Roberto Linares</label>
+						<input type='submit' id='btnSubmit' value='Cerrar Sesion' class='btnSubmit'/>					
+					</div>
+				</p>
+			</form>
+			");
 		}
 		
 		function ShowLoginBox($errorMsg){
 			echo("
-			<form action='index.php?action=login' method='POST' accept-charset='utf-8'>
+			<form id='frmLogin' action='index.php?action=login' method='POST' accept-charset='utf-8'>
 						<p>
 							<div class='LoginBoxFrame'>
+								<label for='txtNombre' class='lblInput'>usuario: </label>
 								<input type='text' id='txtNombre' name='txtNombre' class='txtInput'/>
+								<label for='txtClave' class='lblInput'>clave: </label>
 								<input type='password' id='txtClave' name='txtClave' class='txtInput' />
-								<input type='submit' value='Login' class='btnSubmit'/>							
-							</div>	
+								<input type='submit' id='btnSubmit' value='Iniciar Sesion' class='btnSubmit'/>
 						");
             if($errorMsg != "")
                 echo("<input type='label' class='errorMsg' value='$errorMsg' />");                    
-             echo("</p></form>");
+             echo("</div></p></form>");
 		}
     }		
 ?>
