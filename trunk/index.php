2589aae48ceeb3c8060e2c3229d9711261973ca4
BlendBoy <BlendBoy@68395a18-3848-0410-a3f5-bf879e04881c>
2008-05-10 06:14:07 +0000

    Agregado el estilo, el layout y el html para las paginas espeficicas por usuario. Ya casi todo el disenio principal de las paginas estaria hecho, nada mas haria falta el formato del contenido de cada una de ellas. Continuariamos con el mapa del sitio y la definicion del contenido de cada pagina resultante del mapa.

--- trunk/index.php
+++ trunk/index.php
@@ -5,7 +5,7 @@
 		<meta content="text/html">
         <title>UES FMO - Unidad de Postgrados</title>
 		<link type="text/css" rel="stylesheet" href="Media/main.css">
-		<link type="text/css" rel="stylesheet" href="Media/widgets.css">
+		<link type="text/css" rel="stylesheet" href="Media/transpWidgets.css">
 		<link type="text/css" rel="stylesheet" href="Media/forms.css">
 		<script type="text/javascript" src="mootools.js"></script>
 		<script type="text/javascript" scr="demo.js"></script>
@@ -20,20 +20,34 @@
 						$vuser->Show();
 					?>
 				</div>
-				<!--<div class="WidgetList">
-					<div class="Widget">
-						<div class="WidgetTitle">Estudiantes</h3></div>
+				<div class="WidgetListLeft">
+					<div class="WidgetLeft">
+						<div class="WidgetTitle">Novedades en la Unidad</h3></div>
 						<div class="WidgetContent">
 							<p>widget1</p>
 						</div>
 					</div>
-					<div class="Widget">
-						<div class="WidgetTitle">Docentes</h3></div>
+					<div class="WidgetLeft">
+						<div class="WidgetTitle">Novedades en Foro</h3></div>
 						<div class="WidgetContent">
 							<p>widget2</p>
 						</div>
+					</div>					
+				</div>
+				<div class="WidgetListRight">
+					<div class="WidgetRight">
+						<div class="WidgetTitle">Cursos Actuales y Proximos</h3></div>
+						<div class="WidgetContent">
+							<p>widget3</p>
+						</div>
 					</div>
-				</div>-->
+					<div class="WidgetRight">
+						<div class="WidgetTitle">Acerca de la Unidad</h3></div>
+						<div class="WidgetContent">
+							<p>widget4</p>
+						</div>
+					</div>
+				</div>
     		</div>
     	</div>
     </body>
