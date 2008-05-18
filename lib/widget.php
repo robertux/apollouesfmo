6b47e0670541688a04f87951959791f7521d0ab7
ramayac <ramayac@68395a18-3848-0410-a3f5-bf879e04881c>
2008-05-18 23:31:54 +0000

    Se restauro la funcionalidad (casi) del widget anterior. Se creo WidgetNovedades, que hereda de Widget.

--- lib/widget.php
+++ lib/widget.php
@@ -2,41 +2,61 @@
 include(RUTA."clases/cnovedades.php");
 
 class Widget{
+		public $Contenido; //protected
+		
+		public $Titulo;
+		public $masURL;
+		public $claseCSS;
+		
+		public function Widget($pTitulo="Esta es una Seccion", $pMasLink="http://url.com", $pContenido,$pClaseCSS){
+			$this->Titulo = $pTitulo;
+			$this->Contenido = $pContenido;
+			$this->claseCSS = $pClaseCSS;
+			$this->masURL = $pMasLink;
+		}
+		
+		public function Show($pContenido = ""){
+			echo("<div class='$this->claseCSS'>
+				<div class='WidgetTitle'><a id='TitleText' href='$this->masURL'>$this->Titulo</a></div>
+				<div class='WidgetContent'>");
+				if ($pContenido=="") {
+					echo "<p>$this->Contenido</p>";
+				}
+				else
+					echo "<p>$pContenido</p>";
+			echo("</div><div class='footer'><a href='$this->masURL'>Ver m&aacute;s...</a></div></div>");
+		}
+	}
+
+class WidgetNovedades extends Widget{
 		
 	private $novedades;
-	private $contenido;
-	
-	public $Titulo;
-	public $masURL;
-	public $claseCSS;
 	
 	public function __construct() 
     {
 		$this->novedades = new cNovedades();
-		$this->contenido = "<div class='WidgetContent'><ul>";
-		$this->Poblar();		
+		$this->Contenido = "<div class='WidgetContent'><ul>";
+		$this->Llenar();		
     }
     
-    function Poblar()
+    private function Llenar()
     {
     	$resultado = $this->novedades->GetParaWidget(5);
     	while($row = $resultado->fetch_array())
         {
-        	//titulo, vinculo
-        	//$t = $row[0];
-        	//$v = $row[1];
-        	$this->contenido .= "<li><a href='$row[1]'>$row[0]</a></li>";
+        	//titulo, vinculo //$t = $row[0]; //$v = $row[1];
+        	$this->Contenido .= "<li><a href='$row[1]'>$row[0]</a></li>";
         }
 		$resultado->close();
-        $this->contenido .= "</ul></div>";
+        $this->Contenido .= "</ul></div>";
     }
 
 		
-	function Show(){
+	public function Show(){
 		echo("
 		<div class='$this->claseCSS'>
 		<div class='WidgetTitle'><a id='TitleText' href='$this->masURL'>$this->Titulo</a></div>
-			$this->contenido
+			$this->Contenido
 			<div class='footer'><a href='$this->masURL'>Ver m&aacute;s...</a></div>
 		</div>
 		");
