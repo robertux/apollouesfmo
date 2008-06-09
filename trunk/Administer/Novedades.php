fe80272d4ff2a307a48d4cf0e4129b61c0923b64
ramayac <ramayac@68395a18-3848-0410-a3f5-bf879e04881c>
2008-06-09 18:17:01 +0000

    Agregando algunas modificaciones a Novedades.php para que se integre con el "tema" del sitio.

--- trunk/Administer/Novedades.php
+++ trunk/Administer/Novedades.php
@@ -1,62 +1,69 @@
 <?php
 define("RUTA", realpath("../"));
+require_once(RUTA."/incluye.php");
 require_once(RUTA."/lib/VisualNovedades.php");
 
-/**
-* Prueba del objeto VisualNovedades
-*/
-session_start();
-echo '<html><head><title>apollo -- novedades</title><meta name="generator" http-equiv="content-type" content="text/html"></head><body>';
-echo '<table class="bd" width="100%"><tr><td class="hr"><h2>Editar Noticias de la Unidad de PostGrados</h2></td></tr></table>';
-
-echo "<br><h1>Esta es una pagina de PRUEBA, aun no esta finalizada!</h1><br>";
-
-$visualnov = new VisualNovedades();
-$visualnov->file = "VisualNovedades.php";
-$visualnov->a = @$_GET["a"];
-$visualnov->recid = @$_GET["recid"];
-$visualnov->page = @$_GET["page"];
-$visualnov->sql = @$_POST["sql"];
-
-$visualnov->nov->id = @$_POST["id"];
-$visualnov->nov->titulo = @$_POST["titulo"];
-$visualnov->nov->vinculo = @$_POST["vinculo"];
-$visualnov->nov->descripcion = @$_POST["descripcion"];
-$visualnov->nov->fecha = @$_POST["fecha"];
-
-if (!isset($visualnov->page)) $visualnov->page = 1;
-
-echo "\n<b>DEBUG: accion : $visualnov->a, id : $visualnov->recid, sql : $visualnov->sql </b>";
-echo "descripcion =" . @$_GET["descripcion"] ."\n";
-
-switch ($visualnov->sql) {
-	case "insert":
-		$visualnov->sql_insert();
-		break;
-	case "update":
-		$visualnov->sql_update();
-		break;
-	case "delete":
-		$visualnov->sql_delete();
-		break;
-}
-
-switch ($visualnov->a) {
-	case "add":
-		$visualnov->addrec();
-		break;
-	case "view":
-		$visualnov->viewrec(); //corregir esto
-		break;
-	case "edit":
-		$visualnov->editrec();
-		break;
-	case "del":
-		$visualnov->deleterec();
-		break;
-	default:
-		$visualnov->select();
-		break;
-}
-echo '<table class="bd" width="100%"><tr><td class="hr"><center>Unidad de PostGrados, UES-FMOcc</center></td></tr></table></body></html>';
+$pag = new paginaSecundaria();
+$pag->encabezado();
+$pag->cuerpo();
 ?>
+<div id="wrap">	
+	<?php
+		$vuser = new VisualUsuario();
+		$vuser->Show();
+	?>
+  <div id="header">
+    <div id="logo">
+      <h1>Administer</h1>
+    </div>
+    <ul id="nav">
+      <li><a href="../index.php">Principal</a></li>
+      <li><a href="../Cursos/index.php?opt=mine">Maestrias</a></li>
+      <li><a href="../Forum/index.php?opt=cat">Foro</a></li>
+      <li><a href="../Unidad/index.php?opt=about">La Unidad</a></li>
+    </ul>
+  </div>
+<div id="content"><img src="../Media/style/MainBanner.png" alt="" class="img" />
+	<div id="post"><?php
+			/**
+			* Prueba del objeto VisualNovedades
+			*/
+			//echo "<br><h1>Esta es una pagina de PRUEBA, aun no esta finalizada!</h1><br>";
+			$visualnov = new VisualNovedades();
+			
+			$visualnov->file = "Novedades.php";
+			$visualnov->a = @$_GET["a"];
+			$visualnov->recid = @$_GET["recid"];
+			$visualnov->page = @$_GET["page"];
+			$visualnov->sql = @$_POST["sql"];
+			$visualnov->nov->id = @$_POST["id"];
+			$visualnov->nov->titulo = @$_POST["titulo"];
+			$visualnov->nov->vinculo = @$_POST["vinculo"];
+			$visualnov->nov->descripcion = @$_POST["descripcion"];
+			$visualnov->nov->fecha = @$_POST["fecha"];
+
+			if (!isset($visualnov->page)) $visualnov->page = 1;
+			//echo "\n<b>DEBUG: accion : $visualnov->a, id : $visualnov->recid, sql : $visualnov->sql </b>";
+		//echo "descripcion =" . @$_GET["descripcion"] ."\n";
+			$visualnov->CommitSQL();
+			$visualnov->ShowAction();
+		?></div>
+  </div>
+    <!--<div id="rightMenu">
+	<ul>
+	<li><a href="index.php?opt=about">Acerca de la Unidad</a></li>
+	<li><a href="index.php?opt=proc">Procesos Academicos</a></li>
+	<li><a href="index.php?opt=news">Noticias</a></li>
+	<li><a href="index.php?opt=util">Programas de Utileria</a></li>
+	<li><a href="index.php?opt=contact">Contacto/Suscripcion</a></li>
+	</ul>
+  </div>-->
+    <!-- /content -->
+
+  <!--<div class="clearfix"></div>-->
+  <div id="footer">
+    
+  </div>
+  <!-- /footer -->
+</div>
+<?php $pag->pie(); ?>
