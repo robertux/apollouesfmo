11739edd7dcbfcb4cc03ec6439c87597843bceaa
ramayac <ramayac@68395a18-3848-0410-a3f5-bf879e04881c>
2008-06-29 20:12:25 +0000

    Se removio las caracteristicas de Avatar en el foro, y tambien se elimino la posibilidad de realizar un "cacheado" del mismo. Simplemente ahora usamos más SQL, eso es todo.

--- Forum/include/cache.php
+++ Forum/include/cache.php
@@ -1,32 +1,12 @@
 <?php
 /***********************************************************************
-
-  Copyright (C) 2002-2005  Rickard Andersson (rickard@punbb.org)
-
-  This file is part of PunBB.
-
-  PunBB is free software; you can redistribute it and/or modify it
-  under the terms of the GNU General Public License as published
-  by the Free Software Foundation; either version 2 of the License,
-  or (at your option) any later version.
-
-  PunBB is distributed in the hope that it will be useful, but
-  WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-  MA  02111-1307  USA
-
+Cache file, re coded to disable cache implementation un punBB, by Ramayac.
 ************************************************************************/
 
 
 // Make sure no one attempts to run this script "directly"
 if (!defined('PUN'))
 	exit;
-
 
 //
 // If we are running pre PHP 4.2.0, we add our own implementation of var_export
@@ -78,6 +58,7 @@ if (!function_exists('var_export'))
 //
 function generate_config_cache()
 {
+	/* Estatico en el archivo cache_config.php
 	global $db;
 
 	// Get the forum config from the DB
@@ -92,7 +73,7 @@ function generate_config_cache()
 
 	fwrite($fh, '<?php'."\n\n".'define(\'PUN_CONFIG_LOADED\', 1);'."\n\n".'$pun_config = '.var_export($output, true).';'."\n\n".'?>');
 
-	fclose($fh);
+	fclose($fh);*/
 }
 
 
@@ -111,13 +92,13 @@ function generate_bans_cache()
 		$output[] = $cur_ban;
 
 	// Output ban list as PHP code
-	$fh = @fopen(PUN_ROOT.'cache/cache_bans.php', 'wb');
-	if (!$fh)
-		error('Unable to write bans cache file to cache directory. Please make sure PHP has write access to the directory \'cache\'', __FILE__, __LINE__);
+	//$fh = @fopen(PUN_ROOT.'cache/cache_bans.php', 'wb');
+	//if (!$fh)
+	//	error('Unable to write bans cache file to cache directory. Please make sure PHP has write access to the directory \'cache\'', __FILE__, __LINE__);
 
-	fwrite($fh, '<?php'."\n\n".'define(\'PUN_BANS_LOADED\', 1);'."\n\n".'$pun_bans = '.var_export($output, true).';'."\n\n".'?>');
+	echo '<?php'."\n\n".'define(\'PUN_BANS_LOADED\', 1);'."\n\n".'$pun_bans = '.var_export($output, true).';'."\n\n".'?>';
 
-	fclose($fh);
+	//fclose($fh);
 }
 
 
@@ -136,13 +117,13 @@ function generate_ranks_cache()
 		$output[] = $cur_rank;
 
 	// Output ranks list as PHP code
-	$fh = @fopen(PUN_ROOT.'cache/cache_ranks.php', 'wb');
-	if (!$fh)
-		error('Unable to write ranks cache file to cache directory. Please make sure PHP has write access to the directory \'cache\'', __FILE__, __LINE__);
+	//$fh = @fopen(PUN_ROOT.'cache/cache_ranks.php', 'wb');
+	//if (!$fh)
+		//error('Unable to write ranks cache file to cache directory. Please make sure PHP has write access to the directory \'cache\'', __FILE__, __LINE__);
+//fwrite($fh, 
+	echo '<?php'."\n\n".'define(\'PUN_RANKS_LOADED\', 1);'."\n\n".'$pun_ranks = '.var_export($output, true).';'."\n\n".'?>';
 
-	fwrite($fh, '<?php'."\n\n".'define(\'PUN_RANKS_LOADED\', 1);'."\n\n".'$pun_ranks = '.var_export($output, true).';'."\n\n".'?>');
-
-	fclose($fh);
+	//fclose($fh);
 }
 
 
@@ -170,9 +151,9 @@ function generate_quickjump_cache($group_id = false)
 	while (list(, $group_id) = @each($groups))
 	{
 		// Output quickjump as PHP code
-		$fh = @fopen(PUN_ROOT.'cache/cache_quickjump_'.$group_id.'.php', 'wb');
-		if (!$fh)
-			error('Unable to write quickjump cache file to cache directory. Please make sure PHP has write access to the directory \'cache\'', __FILE__, __LINE__);
+		//$fh = @fopen(PUN_ROOT.'cache/cache_quickjump_'.$group_id.'.php', 'wb');
+		//if (!$fh)
+			//error('Unable to write quickjump cache file to cache directory. Please make sure PHP has write access to the directory \'cache\'', __FILE__, __LINE__);
 
 		$output = '<?php'."\n\n".'if (!defined(\'PUN\')) exit;'."\n".'define(\'PUN_QJ_LOADED\', 1);'."\n\n".'?>';
 		$output .= "\t\t\t\t".'<form id="qjump" method="get" action="viewforum.php">'."\n\t\t\t\t\t".'<div><label><?php echo $lang_common[\'Jump to\'] ?>'."\n\n\t\t\t\t\t".'<br /><select name="id" onchange="window.location=(\'viewforum.php?id=\'+this.options[this.selectedIndex].value)">'."\n";
@@ -196,10 +177,13 @@ function generate_quickjump_cache($group_id = false)
 			$output .= "\t\t\t\t\t\t\t".'<option value="'.$cur_forum['fid'].'"<?php echo ($forum_id == '.$cur_forum['fid'].') ? \' selected="selected"\' : \'\' ?>>'.pun_htmlspecialchars($cur_forum['forum_name']).$redirect_tag.'</option>'."\n";
 		}
 
-		$output .= "\t\t\t\t\t".'</optgroup>'."\n\t\t\t\t\t".'</select>'."\n\t\t\t\t\t".'<input type="submit" value="<?php echo $lang_common[\'Go\'] ?>" accesskey="g" />'."\n\t\t\t\t\t".'</label></div>'."\n\t\t\t\t".'</form>'."\n";
+		$output .= "\t\t\t\t\t".'</optgroup>'."\n\t\t\t\t\t".'</select>'."\n\t\t\t\t\t".'<input type="submit" value="';
+		$output .= $lang_common['Go'];
+		$output .= '" accesskey="g" />'."\n\t\t\t\t\t".'</label></div>'."\n\t\t\t\t".'</form>'."\n";
 
-		fwrite($fh, $output);
+		//fwrite($fh, $output);
+		echo $output;
 
-		fclose($fh);
+		//fclose($fh);
 	}
 }
