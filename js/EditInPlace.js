d0a745d31a84618f25f9467c0637af04a2e74e26
BlendBoy <BlendBoy@68395a18-3848-0410-a3f5-bf879e04881c>
2008-07-03 17:44:02 +0000

    La funcionalidad del edit-in-place parcialmente implementado pero por lo menos los cambios edit-save-cancel en los elementos HTML ya es funcional para los elementos Post. Solo faltaria que por medio de Ajax cargue y guarde las cosas en el servidor, o sea casi nada :p

---
+++ js/EditInPlace.js
@@ -0,0 +1,54 @@
+/**
+ * @author Robertux
+ */
+
+function EditText(idTxt, idBtnAdd, idBtnEdit, idBtnDel, idBtnSave, idBtnCancel){
+	//alert("pase por aca1!. idTxt es " + idTxt);
+	//alert("id: " + idBtnSave);
+	//Cambiamos la clase CSS del control Texto, para que permita modificar su texto
+	document.getElementById(idTxt).disabled = false;
+	document.getElementById(idTxt).className = "innerTitleEdit";
+	document.getElementById(idTxt).focus();
+	//alert("pase por aca2!. idTxt es " + idTxt);
+	//alert("pase por aca3!. idSav es " + idBtnSave);	
+	//alert("pase por aca3!. idCan es " + idBtnCancel);	
+	//Ocultamos el boton de editar
+	//alert("btnEdit es: " + idBtnEdit);
+	if(document.getElementById(idBtnAdd) != null)
+		document.getElementById(idBtnAdd).style.display = "none";
+	if(document.getElementById(idBtnEdit) != null)		
+		document.getElementById(idBtnEdit).style.display = "none";
+	if(document.getElementById(idBtnDel) != null)
+		document.getElementById(idBtnDel).style.display = "none";
+	document.getElementById(idBtnSave).style.display = "inline";
+	document.getElementById(idBtnCancel).style.display = "inline";
+
+}
+
+function SaveText(idTxt, idBtnAdd, idBtnEdit, idBtnDel, idBtnSave, idBtnCancel){
+	document.getElementById(idTxt).disabled = true;	
+	document.getElementById(idTxt).className = "innerTitle";
+	
+	if(document.getElementById(idBtnAdd) != null)
+		document.getElementById(idBtnAdd).style.display = "inline";
+	if(document.getElementById(idBtnEdit) != null)		
+		document.getElementById(idBtnEdit).style.display = "inline";
+	if(document.getElementById(idBtnDel) != null)
+		document.getElementById(idBtnDel).style.display = "inline";
+	document.getElementById(idBtnSave).style.display = "none";
+	document.getElementById(idBtnCancel).style.display = "none";
+}
+
+function CancelText(idTxt, idBtnAdd, idBtnEdit, idBtnDel, idBtnSave, idBtnCancel){
+	document.getElementById(idTxt).disabled = true;	
+	document.getElementById(idTxt).className = "innerTitle";
+	
+	if(document.getElementById(idBtnAdd) != null)
+		document.getElementById(idBtnAdd).style.display = "inline";
+	if(document.getElementById(idBtnEdit) != null)		
+		document.getElementById(idBtnEdit).style.display = "inline";
+	if(document.getElementById(idBtnDel) != null)
+		document.getElementById(idBtnDel).style.display = "inline";
+	document.getElementById(idBtnSave).style.display = "none";
+	document.getElementById(idBtnCancel).style.display = "none";
+}
--- lib/ToolBox.php
+++ lib/ToolBox.php
@@ -1,59 +1,75 @@
 <?php
+	//Caja de controles que muestra los botones de agregar/editar/eliminar/guardar/cancelar en los titulos de los posts
     class ToolBox{
 
 		public $btnAdd;
 		public $btnEdit;
 		public $btnDel;
+		public $btnSave;
+		public $btnCancel;
 		
+		//Constructor
 		public function ToolBox($addEnabled=false, $editEnabled=false, $delEnabled=false){
 			$this->btnAdd = new ToolBoxButton("add", $addEnabled);
 			$this->btnEdit = new ToolBoxButton("edit", $editEnabled);
 			$this->btnDel = new ToolBoxButton("del", $delEnabled);
+			$this->btnSave = new ToolBoxButton("sav", true);
+			$this->btnCancel = new ToolBoxButton("can", true);
 		}
     	
+		//Imprime el control en la pagina
 		public function Show(){
 			echo($this->ToString());
 		}
 		
+		//Genera y devuelve la representacion HTML del control
 		public function ToString(){
 			return "
-				<div id='toolbox'>
-					" . $this->btnAdd->ToString() . $this->btnEdit->ToString() . $this->btnDel->ToString() . "
+				<div class='toolbox'>
+					" . $this->btnAdd->ToString() . $this->btnEdit->ToString() . $this->btnDel->ToString() . $this->btnSave->ToString() . $this->btnCancel->ToString() . "
 				</div>
 			";
 		}
     }
 	
+	//Boton utilizado en un ToolBox
 	class ToolBoxButton{
 		
 		public $id;
+		public $class;
 		public $title;
 		public $imgURL;
 		public $onClick;
 		public $enabled;
 		
-		public function ToolBoxButton($pId="add", $pEnabled=false, $pOnClick="#"){
-			$this->id = $pId;
-			switch($pNombre){
+		//Constructor
+		public function ToolBoxButton($pClass="add", $pEnabled=false, $pOnClick="#"){
+			$this->class = $pClass;
+			$this->id = $this->class;
+			switch($pClass){
 				case "add": $this->title = "agregar";	break;
 				case "edit": $this->title = "editar";	break;
 				case "del": $this->title = "eliminar";	break;
+				case "sav": $this->title = "guardar";	break;
+				case "can": $this->title = "cancelar";	break;
 			}			
 			$this->enabled = $pEnabled;
 			$this->onClick = $pOnClick;
 		}
 		
+		//Imprime el control en la pagina
 		public function Show(){
 			echo($this->ToString());
 		}
 		
+		//Genera y devuelve el HTML que representa al control
 		public function ToString(){
 			if($this->enabled)
 				/*return"
 					<a href='$this->targetURL'><img src='$this->imgURL' alt='$this->alt' /></a>
 				";*/
 				return"
-					<input type='button' title='$this->title' id='$this->id' onClick=\"$this->onClick\" />
+					<input type='button' id='$this->id' title='$this->title' class='$this->class' onClick=\"$this->onClick\" />
 				";
 			else
 				return "";
