7ce25e3a229ae6de0665f89f75afc409602b816a
ramayac <ramayac@68395a18-3848-0410-a3f5-bf879e04881c>
2008-08-07 18:01:22 +0000

    Se integro la conexion del foro con la clase cconexion.php Se agrego documentacion para la instalacion del proyecto.

--- trunk/Forum/config.php
+++ trunk/Forum/config.php
@@ -1,10 +1,21 @@
 <?php
+define("RUTAINC", realpath("../"));
+$foo = RUTAINC."clases/cconexion.php";
+require_once($foo);
+$con = new cConexion();
+#echo "<h1>".$foo."</h1>";
+#echo "<h1> configuracion : ".$con->Db()."</h1>";
+#return 0;
 
 $db_type = 'mysqli';
-$db_host = 'localhost';
-$db_name = 'apollo';
-$db_username = 'apollouser';
-$db_password = 'apollopwd';
+#$db_host = 'localhost';
+#$db_name = 'apollo';
+#$db_username = 'apollouser';
+#$db_password = 'apollopwd';
+$db_host = $con->Host();
+$db_name = $con->Db();
+$db_username = $con->User();
+$db_password = $con->Pass();
 $db_prefix = 'foro_';
 $p_connect = false;
 
