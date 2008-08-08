ec03011f1ce217141a03503fe3e5db4767b1da93
BlendBoy <BlendBoy@68395a18-3848-0410-a3f5-bf879e04881c>
2008-08-08 06:53:59 +0000

    Agregado el archivo tinymceinit.js en el cual se encuentran dos metodos que permiten la inicializacion del TinyMCE en base a una o dos filas, reemplazando a la inicializacion que estaba en el archivo paginaSecundaria.php. Arreglado el problema de activacion/desactivacion de los posts cuando estos contienen tablas internas, ahora para estos casos ademas, se muestra el tinymce con sus botones en dos filas.

---
+++ js/tinymceinit.js
@@ -0,0 +1,47 @@
+/**
+ * @author developer
+ */
+
+function tinymceInitTwoRows(){
+
+	tinyMCE.init({
+		mode: "textareas",
+		
+		
+		theme: "advanced",
+		plugins: "iespell,advimage",
+		
+		theme_advanced_buttons1: "fontselect,fontsizeselect,bold,italic,underline",
+		theme_advanced_buttons2: "justifyleft,justifycenter,justifyright,justifyfull,|,bullist,numlist,|,link,unlink,image,iespell",
+		theme_advanced_buttons3: "",
+		
+		extended_valid_elements: "a[name|href|target|title|onclick],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]",
+		theme_advanced_toolbar_location: "top",
+		theme_advanced_toolbar_align: "left",
+		theme_advanced_statusbar_location: "bottom",
+		theme_advanced_resizing: true,
+	});
+	
+}
+
+function tinymceInitOneRow(){
+
+	tinyMCE.init({
+		mode: "textareas",
+		
+		
+		theme: "advanced",
+		plugins: "iespell,advimage",
+		
+		theme_advanced_buttons1: "fontselect,fontsizeselect,bold,italic,underline,|,justifyleft,justifycenter,justifyright,justifyfull,|,bullist,numlist,|,link,unlink,image,iespell",
+		theme_advanced_buttons2: "",
+		theme_advanced_buttons3: "",
+		
+		extended_valid_elements: "a[name|href|target|title|onclick],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]",
+		theme_advanced_toolbar_location: "top",
+		theme_advanced_toolbar_align: "left",
+		theme_advanced_statusbar_location: "bottom",
+		theme_advanced_resizing: true,
+	});
+	
+}
