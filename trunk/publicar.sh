e8f34a0a58b5a3dc1b5cbdb04c7b6d1f5fb30f6e
ramayac <ramayac@68395a18-3848-0410-a3f5-bf879e04881c>
2008-10-08 05:29:01 +0000

    Modificaciones al infame script, para que no sea tan nocivo, igual arruina el repositorio local svn, pero arregla otras cosas, asi se queda por el momento.

--- trunk/publicar.sh
+++ trunk/publicar.sh
@@ -3,12 +3,6 @@
 #No se realiza ninguna comprobacion extra...
 #ramayac 07/oct/08
 
-echo "Exito al comprimir la carpeta con todo su contenido."
-echo "Generando copia del archivo con nuevo nombre..."
-REV=`svn status --verbose | grep " . " | head -1` #retorna algo como: "32 32 ramayac ./archivo "
-REV=${REV%[0-9]*} #encuentra el primer numero de derecha a izq: "32 3"
-REV=${REV%" "*} #de la cadena anterior, obtiene del primer espacio en blanco hacia la izq: "32"
-
 echo "Eliminando carpetas .svn"
 DIRECTORIOS=`find . -type d -name .svn`
 for DIR in $DIRECTORIOS
@@ -41,8 +35,12 @@ DIR=`pwd`
 tar -cf ./apollo.tar $DIR
 bzip2 -z apollo.tar
 rm apollo.tar
-
-mv apollo.tar.bz Apollo.rev$REV.tar.bz
-echo "Exito al crear: Apollo.rev$REV.tar.bz"
+#echo "Exito al comprimir la carpeta con todo su contenido."
+#echo "Generando copia del archivo con nuevo nombre..."
+#REV=`svn status --verbose | grep " . " | head -1` #retorna algo como: "32 32 ramayac ./archivo "
+#REV=${REV%[0-9]*} #encuentra el primer numero de derecha a izq: "32 3"
+#REV=${REV%" "*} #de la cadena anterior, obtiene del primer espacio en blanco hacia la izq: "32"
+#mv apollo.tar.bz2 Apollo.rev$REV.tar.bz2
+#echo "Exito al crear: Apollo.rev$REV.tar.bz"
 echo "FIN!"
 
